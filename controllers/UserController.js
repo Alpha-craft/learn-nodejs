9998683eaec15bbc78f25b3cea2841274b545f47
Alpha-craft <ainurahman03@gmail.com>
2022-12-09 11:24:28 +0700

    refactor:user search

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,9 +2,28 @@ import User from '../models/User.js'
 
 export const test = async (req,res) => {
   res.send('Test Controller');
+  console.log(req.query)
 }
 
 export const getAllUser = async (req, res) => {
-  const users = await User.findAll()
-   res.json(users);
+  let paramObj = {}
+  if (req.query) {
+    paramObj = {
+      where:req.query
+    }
+    console.log(req.query)
+  }
+
+  const users = await User.findAll(paramObj);
+  res.json(users);
+}
+
+export const getUser =  async (req,res) => {
+  const id = req.params.id
+  const user = await User.findOne({
+    where:{
+      id:id
+    }
+  })
+  res.json(user);
 }
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,9 +1,13 @@
 import express from "express";
-import {test,getAllUser} from '../controllers/UserController.js' //import controller
+import {
+  test,
+  getAllUser,
+  getUser
+} from '../controllers/UserController.js' //import controller
 
 const router = express.Router();
 
 router.get('/test',test)
 router.get('/user',getAllUser)
+router.get('/user/:id',getUser)
 export default router;
-
